ea2ef6b5b9b098a41c714692c67502ef4486e593
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-10-22 14:23:48 +0300

    Делаю первую страницу администрирования добавления тегов

---
+++ TestMitra2/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMitra2.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
