98651e6761df5a5afbfd0108f9d28b250a380433
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-10-22 13:27:15 +0300

    Добавьте файлы проекта.

---
+++ TestMitra2/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestMitra2.Models.Domain;
+
+namespace TestMitra2.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
---
+++ TestMitra2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestMitra2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
